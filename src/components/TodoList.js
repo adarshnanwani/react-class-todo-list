7d228d978b652a82348e97b070e7bdf58a596c93
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-30 18:40:03 +0530

    Add filter feature.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,20 +1,83 @@
 import React, { Component } from 'react';
 import TodoListItem from './TodoListItem';
+import './TodoList.css';
 
 class TodoList extends Component {
+  state = {
+    filter: 'all',
+  };
+  updateFilter = (val) => {
+    this.setState({
+      filter: val,
+    });
+  };
   render() {
-    const itemsJsx = this.props.items.map((item) => (
-      <TodoListItem
-        item={item}
-        key={item.id}
-        delete={this.props.deleteTodo}
-        toggle={this.props.toggleTodo}
-        update={this.props.updateTodo}
-      />
-    ));
+    const itemsJsx = this.props.items
+      .filter((item) => {
+        // if (this.state.filter === 'all') return true;
+        // if (this.state.filter === 'open') {
+        //   if (!item.completed) {
+        //     return true;
+        //   }
+        // }
+        // if (this.state.filter === 'completed') {
+        //   if (item.completed) {
+        //     return true;
+        //   }
+        // }
+        return this.state.filter === 'all'
+          ? true
+          : this.state.filter === 'open' && !item.completed
+          ? true
+          : this.state.filter === 'completed' && item.completed
+          ? true
+          : false;
+      })
+      .map((item) => (
+        <TodoListItem
+          item={item}
+          key={item.id}
+          delete={this.props.deleteTodo}
+          toggle={this.props.toggleTodo}
+          update={this.props.updateTodo}
+        />
+      ));
     return (
-      <div>
+      <div className='TodoList'>
         <h3>Todo List ({this.props.items.length})</h3>
+        <div className='filters'>
+          <span>
+            <input
+              type='radio'
+              name='filter'
+              value='all'
+              id='all'
+              defaultChecked={true}
+              onClick={() =>  this.updateFilter('all')}
+            />
+            <label htmlFor='all'>All</label>
+          </span>
+          <span>
+            <input
+              type='radio'
+              name='filter'
+              value='open'
+              id='open'
+              onClick={() => this.updateFilter('open')}
+            />
+            <label htmlFor='open'>Open</label>
+          </span>
+          <span>
+            <input
+              type='radio'
+              name='filter'
+              value='completed'
+              id='completed'
+              onClick={() => this.updateFilter('completed')}
+            />
+            <label htmlFor='completed'>Completed</label>
+          </span>
+        </div>
         <ul>{itemsJsx}</ul>
       </div>
     );
