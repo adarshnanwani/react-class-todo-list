32bee74701b2b62dfbf7e5e2bcf6b0553d573415
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-04 16:52:56 +0530

    Change App to functional component with hooks.

--- src/App.js
+++ src/App.js
@@ -1,80 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { v4 } from 'uuid';
 import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 import './App.css';
 
-class App extends Component {
-  state = {
-    todos: [],
-    loading: false,
-  };
-  addNewTodo = (value) => {
-    this.setState((prevState) => {
-      return {
-        todos: [
-          ...prevState.todos,
-          {
-            text: value,
-            _id: v4(),
-            completed: false,
-          },
-        ],
-      };
-    });
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const addNewTodo = (value) => {
+    setTodos([
+      ...todos,
+      {
+        text: value,
+        _id: v4(),
+        completed: false,
+      },
+    ]);
   };
 
-  deleteTodo = (id) => {
-    // Update the state
-    this.setState((prevState) => {
-      return {
-        todos: [...prevState.todos].filter((todoItem) => todoItem._id !== id),
-      };
-    });
+  const deleteTodo = (id) => {
+    setTodos([...todos].filter((todoItem) => todoItem._id !== id));
   };
 
-  toggleCompleted = (id) => {
-    this.setState((prevState) => {
-      return {
-        todos: prevState.todos.map((todoItem) => {
-          const item = { ...todoItem };
-          if (item._id === id) {
-            item.completed = !item.completed;
-          }
-          return item;
-        }),
-      };
-    });
-  };
-
-  updateTodo = (id, newText) => {
-    this.setState((prevState) => {
-      return {
-        todos: prevState.todos.map((todoItem) => {
-          const item = { ...todoItem };
-          if (item._id === id) {
-            item.text = newText;
-          }
-          return item;
-        }),
-      };
-    });
-  };
-
-  render() {
-    return (
-      <div className='App'>
-        <h1>Todo List App</h1>
-        <AddTodo addTodo={this.addNewTodo} />
-        <TodoList
-          items={this.state.todos}
-          deleteTodo={this.deleteTodo}
-          toggleTodo={this.toggleCompleted}
-          updateTodo={this.updateTodo}
-        />
-      </div>
+  const toggleCompleted = (id) => {
+    setTodos(
+      [...todos].map((todoItem) => {
+        const item = { ...todoItem };
+        if (item._id === id) {
+          item.completed = !item.completed;
+        }
+        return item;
+      })
     );
-  }
-}
+  };
+
+  const updateTodo = (id, newText) => {
+    setTodos(
+      [...todos].map((todoItem) => {
+        const item = { ...todoItem };
+        if (item._id === id) {
+          item.text = newText;
+        }
+        return item;
+      })
+    );
+  };
+
+  return (
+    <div className='App'>
+      <h1>Todo List App</h1>
+      <AddTodo addTodo={addNewTodo} />
+      <TodoList
+        items={todos}
+        deleteTodo={deleteTodo}
+        toggleTodo={toggleCompleted}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
+};
 
 export default App;
