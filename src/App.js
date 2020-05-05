958ca9982f539e6cab352ac3f0ad7f39424065eb
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-05 10:24:54 +0530

    Add some routing demo.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 import './App.css';
@@ -6,9 +7,14 @@ import './App.css';
 const App = () => {
   return (
     <div className='App'>
-      <h1>Todo List App</h1>
-      <AddTodo />
-      <TodoList />
+      <h1>
+        Todo List App <Link to='/add'>Add</Link>
+        <Link to='/list'>List</Link>
+      </h1>
+      <Switch>
+        <Route path='/add' component={AddTodo} />
+        <Route path='/list' component={TodoList} />
+      </Switch>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
